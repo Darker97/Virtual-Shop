979c511880ec4504795ff1392e2fb93e9fa14788
christian@baltzer.de <christian@baltzer.de>
2019-12-29 22:51:27 +0100

    more logic for the API

--- src/Task 2 - API/ApiLogic.py
+++ src/Task 2 - API/ApiLogic.py
@@ -1,28 +1,46 @@
 import security
+import Database
 
 # ------------------------------------------
 # API logic - Everything the API will do
 
-def QuestionToTheServer(SecurityCookie, QuestionID):
-    if security.CanHeDoThat():
-        return
+def StandartQuestionToTheServer(SecurityCookie, QuestionID, Database, query):
+    if security.CanHeDoThat(SecurityCookie):
+         return Database.SendQuery(query[0], Database)
     else:
         return "Error 413 - Data Forbidden"
 
-def NewData(SecurityCookie, Data, DataID):
-    if security.CanHeDoThat():
-        return
+def SpecialQuestionToTheServer(SecurityCookie, Data ,QuestionID, Database, query):
+    if security.CanHeDoThat(SecurityCookie):
+        if security.InjectionChecker(Data):
+            Database.SendQuery(Data, Database)
+        else:
+            print("POSSIBLE INJECTION")
+            print(Data)
     else:
         return "Error 413 - Data Forbidden"
 
-def NewUser(SecurityCookie, Data):
-    if security.CanHeDoThat():
-        return
+
+def NewData(SecurityCookie, Data, DataID, Database, query):
+    if security.CanHeDoThat(SecurityCookie):
+        Query = (query[1], (Data))
+        return Database.SendQuery(Query, Database)
+    else:
+        return "Error 413 - Data Forbidden"
+
+def NewUser(SecurityCookie, Data, Database, query):
+    if security.CanHeDoThat(SecurityCookie):
+        Query = (query[2], (Data))
+        return Database.SendQuery(Query, Database)
     else:
         return "Error 413 - No Admin"
 
-def Login(Password, UserName):
-    try:
-        return security.SecurityGiveUser()
-    except:
-        return "Error 413 - Not a User"
+def Login(Password, UserName, Database, query):
+    # TODO
+    return security.SecurityGiveUser()
+
+
+def queryloader():
+    File = open("query.sql")
+    query = File.readlines()
+    return query

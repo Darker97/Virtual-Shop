25e1f6e3366fef7c15dca5cebe81b7cc4bfa2ad5
christian@baltzer.de <christian@baltzer.de>
2019-12-29 16:05:22 +0100

    zwischenstop

--- src/Task 2 - API/main.py
+++ src/Task 2 - API/main.py
@@ -1,4 +1,23 @@
+from flask import Flask
+from flask_restful import Api, Resources, reparse
 
-def main:
+# ------------------------------------------
+main()
+
+# ------------------------------------------
+
+
+def main():
     pass
 
+
+class Data(Resources):
+    def get(self, info):
+        return info + "" + "--> Jay, Works :D"
+        pass
+
+    def post(self, Function, Data):
+        pass
+
+    def put(self):
+        pass

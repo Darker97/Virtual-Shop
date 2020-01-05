e1ef397b9e6b77b573dc2539d692cfc2fe7dde22
christian@baltzer.de <christian@baltzer.de>
2020-01-05 23:45:10 +0100

    added Login Funktion

--- src/Task 3 - Customer/ApiHelper.py
+++ src/Task 3 - Customer/ApiHelper.py
@@ -1,9 +1,31 @@
 import requests
+import json
 
 # Sends the given Data to the API
 class ApiHelper:
     def Senddata(self, Adress, body):
+        # add SecurityCookie to body
+        body.add(login())
+
+        Adress += "/review"
 
         # put request
         sended = requests.put(Adress, params=body)
         return sended.json()
+
+    def login(self):
+        # load credentials
+        file = open("credential.key")
+        Data = json.load(file.readline())
+        
+        #UserName Password
+        password = Data['Password']
+        Username = Data['UserName']
+
+        # sends login to API
+        Adress = "/User"
+
+        #UserName, Password
+        body = """{ "UserName" = %s, "Password" = %s }"""
+
+        return requests.put(Adress, params=(body,(Username, password)))

b1050aff7585d1c96323404749c54962e9e2bb1d
christian@baltzer.de <christian@baltzer.de>
2020-01-05 23:45:24 +0100

    added Planing

--- src/Task 3 - Customer/DataLoader.py
+++ src/Task 3 - Customer/DataLoader.py
@@ -3,4 +3,16 @@
 # Collects the Data the Helper needs
 class DataLoader:
     def CollectData(self):
+        # Product Suchen
+
+
+        # nächster Kommentar
+
+        # return Data
+        # Data must contain
+        #  Product, Review
+
+
+
+
         pass

303c9a1e562be8c3bc876d04aff0193ab67ce1f9
christian@baltzer.de <christian@baltzer.de>
2020-01-06 12:18:04 +0100

    load all necessary data

--- src/Task 3 - Customer/DataLoader.py
+++ src/Task 3 - Customer/DataLoader.py
@@ -1,4 +1,6 @@
-
+import ApiHelper
+import random
+import json
 
 # Collects the Data the Helper needs
 class DataLoader:
@@ -10,9 +12,28 @@ class DataLoader:
 
         # return Data
         # Data must contain
-        #  Product, Review
+        # SecurityCookie, Product, Review
+
+        SecurityCookie = ApiHelper.login()
+        Product = getProduct()
+        Review = getReview()
+
+        Body = """{"SecurityCookie" = %s, "Product" = %s, "Review" = %s}  """
+
+        return (Body, (SecurityCookie, Product, Review))
+
+    def getProduct(self) -> String:
+        productsSheet = open("Product_DATA")
+        allJsons = productsSheet.readlines()
+
+        selectedProduct = allJsons[random.randint(0, allJsons.count()-1)]
+
+        Json = json.load(selectedProduct)
+        return Json['name']
+        
 
 
-
-
-        pass
+    def getReview(self) -> String:
+        AllComments = open("comments.csv").readlines()
+        return AllComments[random.randint(0, AllComments.count)]
+        

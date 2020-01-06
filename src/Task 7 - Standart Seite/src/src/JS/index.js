95edf12b0af6a46e7dca0e5e1dc0800ab1fe6f2a
christian@baltzer.de <christian@baltzer.de>
2020-01-06 22:40:05 +0100

    init of JS

---
+++ src/Task 7 - Standart Seite/src/src/JS/index.js
@@ -0,0 +1,28 @@
+/**
+ * Does Everything that controlls the Website
+ */
+
+function login(params) {
+
+    //Check for Input
+
+    // Load Input
+
+    // send Request to API via API.js
+
+    // Check answer 
+
+    // Save Answer as Cookie
+    
+}
+
+function loadData(params) {
+
+
+    // Load Data
+
+    // Check for Cookie
+
+    // if Cookie is there, load special Data
+    
+}

e6f3baa22315a2a949b595c225911974472947e2
elephant12 <caro.brueckmann@web.de>
2020-01-07 16:40:03 +0100

    error import js-sha3 library
    hashing of username and password

--- src/Task 7 - Standart Seite/src/JS/index.js
+++ src/Task 7 - Standart Seite/src/JS/index.js
@@ -1,14 +1,19 @@
 /**
  * Does Everything that controlls the Website
  */
+
+//import doesnt work "TypeError: Error resolving module specifier: js-sha3"
+import * as sha from 'js-sha3'
+
 console.log('test')
 console.log(document.getElementById('username1').innerText + ' yep')
 login()
-function login () { //script on load login in html!!
+function login () { // script on load login in html!!
   // Check for Input
   // let button = document.getElementsByTagName('button')
   document.getElementById('button1').addEventListener('click', function () {
-    loadInput()
+    // loadInput()
+    hashLoginData()
   })
 
   // Load Input
@@ -20,7 +25,16 @@ function login () { //script on load login in html!!
     return [username, password]
   }
 
-  // HASH Input with Library: https://github.com/emn178/js-sha3
+  // HASH Input with Library: https://github.com/emn178/js-sha3    Use sha3_256
+  
+   
+
+  function hashLoginData() {
+    var sha3_256 = require('js-sha3').sha3_256
+    let usernameHashed = sha.sha3_256(loadInput()[0])
+    let passwordHashed = sha.sha3_256(loadInput()[1])
+    console.log(usernameHashed)
+  }
 
   // send Request to API via API.js
 

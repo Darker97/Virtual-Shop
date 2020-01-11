4ec4815239e01285d68d58aa86f1ef590a22504e
elephant12 <caro.brueckmann@web.de>
2020-01-11 14:36:03 +0100

    API stuff

--- src/Task 7 - Standart Seite/src/JS/index.js
+++ src/Task 7 - Standart Seite/src/JS/index.js
@@ -6,7 +6,7 @@ import * as data from './Data.js'
 /**
  * Does Everything that controlls the Website
  */
-
+var adress = 'localhost:5000/Data'
 // import doesnt work "TypeError: Error resolving module specifier: js-sha3"
 // import * as sha from 'js-sha3'
 
@@ -19,20 +19,20 @@ loadData()
 
 
 function login() { // script on load login in html!!
-  // Check for Input
-  document.getElementById('button1').addEventListener('click', function () {
-    loadInput()
-    // hashLoginData()
-  })
+    // Check for Input
+    document.getElementById('button1').addEventListener('click', function () {
+        loadInput()
+        // hashLoginData()
+    })
 
-// Load Input
+    // Load Input
     function loadInput() {
         let username = document.getElementById('username1').value
         let password = document.getElementById('password1').value
         console.log(username)
         console.log(password)
         return [username, password]
-    }    
+    }
 
     // HASH Input with Library: https://github.com/emn178/js-sha3    Use sha3_256
 
@@ -57,21 +57,58 @@ function loadData(params) {
 
     // Load Data
     // test
+   /* let message = {
+        SecurityCookie: 'cookie',
+        QuestionID: 0
+    }
+    let answer = request.Get(adress, message)
+    let aLength = answer.length
+    console.log(answer)
     let i = 0
-    while(i !== 5) {
+    while (i !== aLength) {
+        let product = arr[i]
+        product.split(',')
         let view = ui.view()
-        let name = ui.headline('name')
-        let price =ui.headline3('price')
-        let text = ui.content('lalalala')
+        let name = ui.headline(product[1])
+        let price = ui.headline3(product[11] + '€')
+        let text = ui.content('Calories: ' + product[3] + '\nProtein: ' + product[4] + '\nFat: ' + product[5] + '\nSodium: ' + product[6] + '\nFiber: ' + product[7] + '\nCarbo: ' + product[8] + '\nSugar: ' + product[9] + '\nVitamins: ' + product[10])
         let button = ui.button('See more')
         document.getElementById('grid').appendChild(view)
         document.querySelectorAll('artikel')[i].appendChild(name)
         document.querySelectorAll('artikel')[i].appendChild(price)
         document.querySelectorAll('artikel')[i].appendChild(text)
         document.querySelectorAll('artikel')[i].appendChild(button)
+        document.querySelectorAll('button')[1].addEventListener('click', function () {
+            let storage = ui.headline3('10x in Storage')  //muss natürlich noch individualisiert werden
+            document.querySelectorAll('artikel')[1+i].appendChild(storage)
+        })
         i++
     }
+*/
+    //tests
+    let i = 0
 
+    while( i != 10) {
+        let view = ui.view()
+        let name = ui.headline('headline')
+        let price = ui.headline3('10€')
+        let text = ui.content('Calories: 4 \nProtein: \nFat: 4 \nSodium:4 \nFiber: 4 \nCarbo: 44 \nSugar: 4 \nVitamins: 5')
+        let button = ui.button('See more')
+        document.getElementById('grid').appendChild(view)
+        document.querySelectorAll('artikel')[i].appendChild(name)
+        document.querySelectorAll('artikel')[i].appendChild(price)
+        document.querySelectorAll('artikel')[i].appendChild(text)
+        document.querySelectorAll('artikel')[i].appendChild(button)
+        document.querySelectorAll('button')[i+1].addEventListener('click', function () { //auslagern!
+            let storage = ui.headline3('10x in Storage')
+            //document.querySelectorAll('artikel')[i].appendChild(storage)
+            console.log(i)
+            
+        })
+        i++
+
+    }
+    
     // Check for Cookie
 
     // if Cookie is there, load special Data

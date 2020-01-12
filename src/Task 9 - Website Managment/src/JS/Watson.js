3a3fd45d35ad032961e3fb33b9777eb451c04050
christian@baltzer.de <christian@baltzer.de>
2020-01-12 14:04:34 +0100

    added audioRecording

--- src/Task 9 - Website Managment/src/JS/Watson.js
+++ src/Task 9 - Website Managment/src/JS/Watson.js
@@ -1,4 +1,17 @@
 
+// credentials
+var keys = {
+  speechToText: [
+    {
+      name: 'virtual-shop---watso-speechtotext-1578734019631',
+      credentials: {
+        apikey: 'giWlFoTiwAuEWngH6QUdyskpPY52s8fJx-5OaEk2bMei',
+        url: 'https://api.eu-de.speech-to-text.watson.cloud.ibm.com/instances/bf66765f-e5f1-4f4a-82ec-bdacd76a1af7'
+      }
+    }
+  ]
+}
+
 // record voice
 var recorder, gumStream
 var recordButton = document.getElementById('recordButton')
@@ -9,24 +22,30 @@ function toggleRecording () {
     recorder.stop()
     gumStream.getAudioTracks()[0].stop()
   } else {
-    navigator.mediaDevices.getUserMedia({
-      audio: true
-    }).then(function (stream) {
-      gumStream = stream
-      recorder = new MediaRecorder(stream)
-      recorder.ondataavailable = function (e) {
-        var url = URL.createObjectURL(e.data)
-        var preview = document.createElement('audio')
-        preview.controls = true
-        preview.src = url
-        document.body.appendChild(preview)
-      }
-      recorder.start()
-    })
+    try {
+      navigator.mediaDevices.getUserMedia({
+        audio: true
+      })
+        .then(function (stream) {
+          gumStream = stream
+          recorder = new MediaRecorder(stream)
+          recorder.ondataavailable = function (e) {
+            var url = URL.createObjectURL(e.data)
+            var preview = document.createElement('audio')
+            preview.controls = true
+            preview.src = url
+            document.getElementById('Watson').appendChild(preview)
+          }
+          recorder.start()
+        })
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
 // send to API
+// 'audio/ogg' in Firefox
 
 // Result + Logic
 

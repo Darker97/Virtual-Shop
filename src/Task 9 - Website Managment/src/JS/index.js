f48d15b3d7353ee78f71a3ab0acc8d008733dc96
christian@baltzer.de <christian@baltzer.de>
2020-01-12 11:00:53 +0100

    finished Webpage
    - added new graphs
    - fixed Bug in request

--- src/Task 9 - Website Managment/src/JS/index.js
+++ src/Task 9 - Website Managment/src/JS/index.js
@@ -1,5 +1,6 @@
 import * as UI from './UI.js'
 import { Get } from './request.js'
+import * as CanvasJS from 'canvasjs'
 
 /**
  * Does Everything that controlls the Website
@@ -14,23 +15,35 @@ function loadCharts (params) {
   var Work = document.getElementById('One')
   One(Work)
 
-  Work = document.getElementById('Two')
-  Two(Work)
+  setTimeout(function() {
+    Work = document.getElementById('Two')
+    Two(Work)
+  }, 1000)
+  
+  setTimeout(function() {
+    Work = document.getElementById('Three')
+    Three(Work)
+  }, 1000)
 
-  Work = document.getElementById('Three')
-  Three(Work)
+  setTimeout(function() {
+    Work = document.getElementById('Four')
+    Four(Work)
+  }, 1000)
 
-  Work = document.getElementById('Four')
-  Four(Work)
+  setTimeout(function() {
+    Work = document.getElementById('Five')
+    Five(Work)
+  }, 1000)
 
-  Work = document.getElementById('Five')
-  Five(Work)
+  setTimeout(function() {
+    Work = document.getElementById('Six')
+    Six(Work)
+  }, 1000)
 
-  Work = document.getElementById('Six')
-  Six(Work)
-
-  Work = document.getElementById('Seven')
-  Seven(Work)
+  setTimeout(function() {
+    Work = document.getElementById('Seven')
+    Seven(Work)
+  }, 1000)
 }
 
 /**
@@ -45,10 +58,32 @@ function One (WorkingObject) {
   var Data = Get(Adress, form)
 
   // Returns: Name, Price, charge
+  var Temp = Data.6
+  var datapointsTemp = []
+  Temp.forEach(element => datapointsTemp.push({y:element.Rating, label: Products_Name}));
 
-  // TODO: USE THAT
-  console.log(Data)
-  return ''
+
+  var chart = new CanvasJS.Chart("One", {
+	animationEnabled: false,
+	
+	axisX:{
+		interval: 1
+	},
+	axisY2:{
+		interlacedColor: "rgba(1,77,101,.2)",
+		gridColor: "rgba(1,77,101,.1)",
+		title: "Names"
+	},
+	data: [{
+		type: "bar",
+		name: "Bought",
+		axisYType: "secondary",
+		color: "#014D65",
+		dataPoints: datapointsTemp
+	}]
+    })
+chart.render();
+
 }
 
 // what is rated highest
@@ -61,9 +96,31 @@ function Two (WorkingObject) {
 
   // Returns: Name, Score
 
-  // TODO: USE THAT
-  console.log(Data)
-  return ''
+  var Temp = Data.7
+  var datapointsTemp = []
+  Temp.forEach(element => datapointsTemp.push({y:element.Score, label: element.Name}));
+
+
+  var chart = new CanvasJS.Chart("Two", {
+	animationEnabled: false,
+	
+	axisX:{
+		interval: 1
+	},
+	axisY2:{
+		interlacedColor: "rgba(1,77,101,.2)",
+		gridColor: "rgba(1,77,101,.1)",
+		title: "Names"
+	},
+	data: [{
+		type: "bar",
+		name: "Rating",
+		axisYType: "secondary",
+		color: "#014D65",
+		dataPoints: datapointsTemp
+	}]
+    })
+chart.render();
 }
 
 // What do we have in stock
@@ -75,9 +132,42 @@ function Three (WorkingObject) {
   var Data = Get(Adress, form)
   // returns Name and charge
 
+  var Temp = Data.8
+
   // TODO: USE THAT
-  console.log(Data)
-  return ''
+  var newtablle = document.createElement("table")
+  var newLine = document.createElement("tr")
+
+  // Add the first line
+  var newName = document.createElement("th")
+  newName.innerText = "Name"
+  newLine.appendChild(newName)
+
+  var newCharge = document.createElement("th")
+  newCharge.innerText = "Charge"
+  newCharge.appendChild(newName)
+
+  newtablle.appendChild(newLine)
+
+  // Load Names
+  var NameRow = document.createElement("tr")
+  Temp.forEach(element => {
+      var tempElement = document.createElement("th")
+      tempElement.innerText = element.Name
+      NameRow.appendChild(tempElement)
+  })
+  newtablle.appendChild(NameRow)
+
+  // Load charge
+  var ChargeRow =  document.createElement("tr")
+  Temp.forEach(element => {
+    var tempElement = document.createElement("th")
+    tempElement.innerText = element.Name
+    ChargeRow.appendChild(tempElement)
+  })
+  newtablle.appendChild(ChargeRow)
+
+  WorkingObject.appendChild(newtablle)
 }
 
 // Product with the most comments
@@ -90,9 +180,30 @@ function Four (WorkingObject) {
 
   // Returns Name and Charge
 
-  // TODO: USE THAT
-  console.log(Data)
-  return ''
+  var Temp = Data.9
+  var datapointsTemp = []
+  Temp.forEach(element => datapointsTemp.push({y:element.Charge, label: element.Name}));
+
+
+  var chart = new CanvasJS.Chart("Four", {
+	animationEnabled: false,
+	
+	axisX:{
+		interval: 1
+	},
+	axisY2:{
+		interlacedColor: "rgba(1,77,101,.2)",
+		gridColor: "rgba(1,77,101,.1)",
+		title: "Names"
+	},
+	data: [{
+		type: "bar",
+		name: "Charge",
+		axisYType: "secondary",
+		color: "#014D65",
+		dataPoints: datapointsTemp
+	}]
+    })
 }
 
 // User who made the most Comments
@@ -103,34 +214,75 @@ function Five (WorkingObject) {
   form.append('QuestionID', QuestionId)
   var Data = Get(Adress, form)
 
-  // TODO: USE THAT
-  console.log(Data)
-  return ''
-}
+  // Return name, charge
+
+  var Temp = Data.10
+  var datapointsTemp = []
+  Temp.forEach(element => datapointsTemp.push({y:element.charge, label: element.name}));
+
+
+  var chart = new CanvasJS.Chart("Five", {
+	animationEnabled: false,
+	
+	axisX:{
+		interval: 1
+	},
+	axisY2:{
+		interlacedColor: "rgba(1,77,101,.2)",
+		gridColor: "rgba(1,77,101,.1)",
+		title: "Names"
+	},
+	data: [{
+		type: "bar",
+		name: "charge",
+		axisYType: "secondary",
+		color: "#014D65",
+		dataPoints: datapointsTemp
+	}]
+    })
 
 // Umsatz pro Produkt
 function Six (WorkingObject) {
-  var QuestionId = 10
+  var QuestionId = 11
   var form = new FormData()
   form.append('SecurityCookie', 'dasd')
   form.append('QuestionID', QuestionId)
   var Data = Get(Adress, form)
 
-  // TODO: USE THAT
-  console.log(Data)
-  return ''
+  var Temp = Data.11
+  var datapointsTemp = []
+  Temp.forEach(element => datapointsTemp.push({y:element.Revenue, label: element.Name}));
+
+
+  var chart = new CanvasJS.Chart("Six", {
+	animationEnabled: false,
+	
+	axisX:{
+		interval: 1
+	},
+	axisY2:{
+		interlacedColor: "rgba(1,77,101,.2)",
+		gridColor: "rgba(1,77,101,.1)",
+		title: "Names"
+	},
+	data: [{
+		type: "bar",
+		name: "Revenue",
+		axisYType: "secondary",
+		color: "#014D65",
+		dataPoints: datapointsTemp
+	}]
+    })
 }
 
 // GesamtUmsatz
 function Seven (WorkingObject) {
-  var QuestionId = 10
+  var QuestionId = 12
   
   var form = new FormData()
   form.append('SecurityCookie', 'dasd')
   form.append('QuestionID', QuestionId)
   var Data = Get(Adress, form)
 
-  // TODO: USE THAT
-  console.log(Data)
-  return ''
+  var newHeadline = UI.headline(Data.12[0].Revenue)
 }
